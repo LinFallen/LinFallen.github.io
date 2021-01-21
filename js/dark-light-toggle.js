3752904f6052ee385fd8a9462a46a5d89d1ebdac
LinFallen <1664691058@qq.com>
2021-01-21 11:31:25 +0800

    Site updated: 2021-01-21 11:31:24

---
+++ js/dark-light-toggle.js
@@ -0,0 +1,73 @@
+KEEP.initModeToggle = () => {
+
+  KEEP.utils.modeToggle = {
+
+    localStorageKey: 'KEEP',
+    modeToggleButton_dom: document.querySelector('.tool-dark-light-toggle'),
+    iconDom: document.querySelector('.tool-dark-light-toggle i'),
+    articleContent: document.querySelector('.main-content'),
+
+    setItemUtil(modeClass, prefersColorScheme) {
+      document.body.classList.toggle(modeClass);
+      const isDark = document.body.className.indexOf(modeClass) === -1;
+
+      if (isDark) {
+        this.iconDom.className = 'fas fa-moon';
+        this.articleContent.classList.remove('night-code-theme');
+      } else {
+        this.iconDom.className = 'fas fa-sun';
+        this.articleContent.classList.add('night-code-theme');
+      }
+      localStorage.setItem(this.localStorageKey, JSON.stringify(
+        {
+          prefersColorScheme: prefersColorScheme,
+          isDark: isDark
+        }
+      ));
+    },
+
+    initModeStatus() {
+      this.modeConfig = JSON.parse(localStorage.getItem(this.localStorageKey));
+      if (this.modeConfig) {
+        if (this.modeConfig.prefersColorScheme === 'dark') {
+          if (this.modeConfig.isDark) {
+            document.body.classList.remove('light-mode');
+            this.articleContent.classList.remove('night-code-theme');
+            this.iconDom.className = 'fas fa-sun';
+          } else {
+            document.body.classList.add('light-mode');
+            this.articleContent.classList.add('night-code-theme');
+            this.iconDom.className = 'fas fa-moon';
+          }
+        } else {
+
+          if (this.modeConfig.isDark) {
+            document.body.classList.remove('dark-mode');
+            this.articleContent.classList.remove('night-code-theme');
+            this.iconDom.className = 'fas fa-moon';
+          } else {
+            document.body.classList.add('dark-mode');
+            this.articleContent.classList.add('night-code-theme');
+            this.iconDom.className = 'fas fa-sun';
+          }
+
+        }
+
+      }
+    },
+
+    initModeToggleButton() {
+      this.modeToggleButton_dom.addEventListener('click', () => {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          this.setItemUtil('light-mode', 'dark');
+        } else {
+          this.setItemUtil('dark-mode', 'light');
+        }
+      });
+    }
+  }
+
+  KEEP.utils.modeToggle.initModeStatus();
+  KEEP.utils.modeToggle.initModeToggleButton();
+
+};
---
+++ js/toc.js
@@ -0,0 +1,87 @@
+function initTOC() {
+  KEEP.utils.navItems = document.querySelectorAll('.post-toc-wrap .post-toc li');
+
+  if (KEEP.utils.navItems.length > 0) {
+
+    KEEP.utils = {
+
+      ...KEEP.utils,
+
+      findActiveIndexByTOC() {
+        if (!Array.isArray(KEEP.utils.sections)) return;
+        let index = KEEP.utils.sections.findIndex(element => {
+          return element && element.getBoundingClientRect().top - 20 > 0;
+        });
+        if (index === -1) {
+          index = KEEP.utils.sections.length - 1;
+        } else if (index > 0) {
+          index--;
+        }
+        this.activateNavByIndex(index);
+      },
+
+      registerSidebarTOC() {
+        KEEP.utils.sections = [...document.querySelectorAll('.post-toc li a.nav-link')].map(element => {
+          const target = document.getElementById(decodeURI(element.getAttribute('href')).replace('#', ''));
+          element.addEventListener('click', event => {
+            event.preventDefault();
+            const offset = target.getBoundingClientRect().top + window.scrollY;
+            window.anime({
+              targets: document.scrollingElement,
+              duration: 500,
+              easing: 'linear',
+              scrollTop: offset - 10,
+              complete: function () {
+                setTimeout(() => {
+                  KEEP.utils.pageTop_dom.classList.add('hide');
+                }, 100)
+              }
+            });
+          });
+          return target;
+        });
+      },
+
+      activateNavByIndex(index) {
+        const target = document.querySelectorAll('.post-toc li a.nav-link')[index];
+        if (!target || target.classList.contains('active-current')) return;
+
+        document.querySelectorAll('.post-toc .active').forEach(element => {
+          element.classList.remove('active', 'active-current');
+        });
+        target.classList.add('active', 'active-current');
+        let parent = target.parentNode;
+        while (!parent.matches('.post-toc')) {
+          if (parent.matches('li')) parent.classList.add('active');
+          parent = parent.parentNode;
+        }
+        // Scrolling to center active TOC element if TOC content is taller then viewport.
+        const tocElement = document.querySelector('.post-toc-wrap');
+        window.anime({
+          targets: tocElement,
+          duration: 200,
+          easing: 'linear',
+          scrollTop: tocElement.scrollTop - (tocElement.offsetHeight / 2) + target.getBoundingClientRect().top - tocElement.getBoundingClientRect().top
+        });
+      },
+
+      showPageAsideWhenHasTOC() {
+        KEEP.utils.leftSideToggle.toggleBar.style.display = 'flex';
+        KEEP.utils.leftSideToggle.isOpenPageAside = true;
+        KEEP.utils.leftSideToggle.changePageLayoutWhenOpenToggle(KEEP.utils.leftSideToggle.isOpenPageAside);
+      }
+    }
+
+    KEEP.utils.showPageAsideWhenHasTOC();
+    KEEP.utils.registerSidebarTOC();
+
+  } else {
+    KEEP.utils.pageContainer_dom.removeChild(document.querySelector('.page-aside'));
+  }
+}
+
+if (KEEP.theme_config.pjax.enable === true && KEEP.utils) {
+  initTOC();
+} else {
+  window.addEventListener('DOMContentLoaded', initTOC);
+}
